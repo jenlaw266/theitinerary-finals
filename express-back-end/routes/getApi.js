87ce5c1865ac64e9830ba4e30e6777f53d9b5d42
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-23 17:15:25 +0000

    working on api call

---
+++ express-back-end/routes/getApi.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const axios = require('axios');
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+const getApi = async function() {
+  let output = []
+    let response = await axios
+      .get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=london+tourist+attraction&language=en&key=${REACT_APP_API_KEY}`)
+      .then((response) => {
+        output = response.data.results
+      });
+      return output
+};
+
+module.exports = getApi;
--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
 
       console.log(response.data.message) // Just the message
       this.setState({
-        message: response.data.message
+        message: response.data.message,
+        act: response.data.act[5].name
       });
     }) 
   }
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{ this.state.message }</h1>
+        <h1>{ this.state.act }</h1>
         <button onClick={this.fetchData} >
           Fetch Data
         </button>        
