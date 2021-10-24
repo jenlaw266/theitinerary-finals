5d4ff4973137433966950fd958eb2587b815ed0c
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-24 17:13:20 +0000

    working on added api to database

--- express-back-end/routes/getActivities.js
+++ express-back-end/routes/getActivities.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
 const getApi = require('./getApi');
 
-const getActivities = async function() {
+const getActivities = async function(db) {
+  const queryString =  `INSERT INTO activities(name, location, lat, long, heart, day_id, itinerary_id)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
   let output = []
   await getApi()
   .then((response) => {
     for (let i = 0; i < response.length; i++) {
+      let name = response[i].name
+      let location = 'London'
+      let address = response[i].formatted_address
+      let lat = response[i].geometry.location.lat
+      let long = response[i].geometry.location.lng
+      let rating = response[i].rating
       output.push({
-        name: response[i].name,
-        address: response[i].formatted_address,
-        lat: response[i].geometry.location.lat,
-        lng: response[i].geometry.location.lng,
-        rating: response[i].rating
+        name: name,
+        location: location,
+        address: address,
+        lat: lat,
+        long: long,
+        rating: rating
       })
     }
   })
--- express-back-end/server.js
+++ express-back-end/server.js
@@ -4,6 +4,10 @@ const BodyParser = require('body-parser');
 const getActivities = require('./routes/getActivities');
 const PORT = 8080;
 
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
@@ -11,7 +15,7 @@ App.use(Express.static('public'));
 
 // Sample GET route
 App.get('/api/activities', async function(req, res) {
-  const activities = await getActivities();
+  const activities = await getActivities(db);
   res.json({
     message: 'Success, able to get data from api',
     act: activities
