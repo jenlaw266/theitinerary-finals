d4d3ac6af67c8fd2dd9c5aac9c7d9dee99182faa
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-26 20:39:59 +0000

    added logic to use the referenece image string and call api to recive url string

--- express-back-end/routes/getImage.js
+++ express-back-end/routes/getImage.js
@@ -1,15 +1,29 @@
-const axios = require('axios')
+const axios = require('axios');
+const { query } = require('express');
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
-const getImage = async function(activities) {
-  let image = null
-  axios.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${imageString}&sensor=false&key=AIzaSyDbGykD5qCfrjRre1A0l8b4JrV3x8PDz60`)
-  .then((response) => {
-    image = response.data
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-  return image
+const getImage = async function(db, activities) {
+  output = [];
+  for (const activity of activities) {
+    imageString = activity.image
+    if (imageString !== "null") {
+      await axios.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${imageString}&sensor=false&key=${REACT_APP_API_KEY}`)
+      .then((response) => {
+        const url = response.request._redirectable._options.href
+
+          activity.image = url
+          output.push(activity)
+          db.query(`UPDATE activities SET image = $1 WHERE name = $2;`, [url, activity.name])
+          .catch(error =>{
+            console.log(error)
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+  return output
 };
 
 module.exports = getImage;
