2e06a1a65784c92f6bc9c5867f13a6939656e802
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-26 00:24:17 +0000

    finished logic to grab all the itineraries from the database and out put it as an array

--- express-back-end/routes/getAllItineraries.js
+++ express-back-end/routes/getAllItineraries.js
@@ -1,4 +1,11 @@
-const getAllItineraries = (db) => {
-  const dbQuery = db.query(`SELECT * From itineraries;`)
-  console.log('dbQuery', dbQuery);
+const getAllItineraries = async function (db) {
+  const dbQuery = await db.query(`SELECT * From itineraries;`)
+  const trips = dbQuery.rows
+  const output = []
+  for (const trip of trips) {
+    output.push(trip)
+  }
+  return output
 };
+
+module.exports = getAllItineraries;
